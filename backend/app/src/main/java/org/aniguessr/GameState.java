887dcb470fc1a43d64055a01142be35e1f8b330d
mooreli104 <moore_li4@outlook.com>
2026-06-28 21:43:11 -0400

    feat: create gamestate enum for state machine

--- backend/app/src/main/java/org/aniguessr/GameState.java
+++ backend/app/src/main/java/org/aniguessr/GameState.java
@@ -1,8 +1,34 @@
 package org.aniguessr;
 
+
 public enum GameState {
     LOBBY{
+        @Override
+        public GameState nextState() {
+            return ROUND_ACTIVE;
+        }
+    },
+    ROUND_ACTIVE{
+         @Override
+        public GameState nextState() {
+            return ROUND_SCORING;
+        }
 
-        
-    }
+    },
+    ROUND_SCORING{
+        @Override
+        public GameState nextState() {
+            return GAME_OVER;
+        }
+
+    },
+    GAME_OVER{
+        @Override
+        public GameState nextState() {
+            return this;
+        }
+    };
+
+    public abstract GameState nextState(); 
+
 }
