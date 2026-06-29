9c92f49083a90d8ee5c452ae3b816326000c11df
mooreli104 <moore_li4@outlook.com>
2026-06-28 21:53:25 -0400

    refactor: remove host parameter from constructor, now handled in method

--- backend/app/src/main/java/org/aniguessr/Room.java
+++ backend/app/src/main/java/org/aniguessr/Room.java
@@ -1,48 +1,52 @@
 package org.aniguessr;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class Room {
     private final String id;
-    private Map<String, Player> connectedPlayers;
+    private final Map<String, Player> connectedPlayers;
     private String host;
     private GameState state;
     private int round;
     private Anime anime;
 
-    public Room(String host) {
+    public Room() {
         this.id = UUID.randomUUID().toString();
-        this.connectedPlayers = new ConcurrentHashMap<>();
-        this.host = host;
+        this.connectedPlayers = new HashMap<>();
+        this.host = null;
         this.state = GameState.LOBBY;
         this.round = 1;
         this.anime = new Anime();
     }
 
-    public String getId() {
-        return this.id;
+    public String getId() { return id; }
+    public String getHost() { return host; }
+    public GameState getState() { return state; }
+    public int getRound() { return round; }
+
+    public void addPlayer(Player player) {
+        if (connectedPlayers.isEmpty()) {
+            this.host = player.getId();
+        }
+        connectedPlayers.put(player.getId(), player);
     }
 
-     public void addPlayer(String id, Player player) {
-        this.connectedPlayers.put(id, player);
+    public void removePlayer(String playerId) {
+        connectedPlayers.remove(playerId);
+        if (connectedPlayers.isEmpty()) {
+            host = null;
+        } else if (playerId.equals(host)) {
+            host = connectedPlayers.keySet().iterator().next();
+        }
     }
 
-    public void removePlayer(String id) {
-        this.connectedPlayers.remove(id);
+    public Map<String, Player> getPlayers() {
+        return Map.copyOf(connectedPlayers);
     }
 
-    public Map<String, Player> getRoomSnapshot() {
-        return Map.copyOf(this.connectedPlayers);
-    }
-
-    public boolean isEmpty(){
-        return this.connectedPlayers.isEmpty();
-    }
-
-    @Override
-    public String toString() {
-        return this.connectedPlayers.toString();
+    public boolean isEmpty() {
+        return connectedPlayers.isEmpty();
     }
 }
